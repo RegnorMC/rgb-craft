12a81a04e4e88563dcc5b25b6c6437c9a30750b3
KubaglikoPL <30959476+KubaglikoPL@users.noreply.github.com>
2021-04-11 18:23:27 +0200

    Update RGBCraft.java

--- src/main/java/net/regnormc/rgbcraft/RGBCraft.java
+++ src/main/java/net/regnormc/rgbcraft/RGBCraft.java
@@ -4,8 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class RGBCraft {
-	public static final String MOD_ID = "mod-info-cmd";
-	public static final Logger LOGGER = LogManager.getLogger("Mod Info Cmd");
+	public static final String MOD_ID = "rgb-craft";
+	public static final Logger LOGGER = LogManager.getLogger("RGBCraft");
 
 	public static void init() {
 	}
